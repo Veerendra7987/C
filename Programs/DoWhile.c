b568d0bef1379aefd58b44b34c5ed2970dbe0760
Veerendra7987 <ashuahirwar512@gmail.com>
2025-09-28 21:52:17 +0530

    Create DoWhile.c

---
+++ Programs/DoWhile.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    do {
+        printf("Enter a number (0 to stop): ");
+        scanf("%d", &num);
+
+        sum += num;  
+
+    } while (num != 0);   
+
+    printf("Total sum = %d\n", sum);
+
+    return 0;
+}
